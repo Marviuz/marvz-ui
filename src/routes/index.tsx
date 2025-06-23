b1077b5b0cbccd54e2f8bb52dafdc3d942cd988a
Marviuz <marviuz.dev@gmail.com>
2025-06-23 22:04:24 +0800

    chore: add shadcn

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,3 +1,4 @@
+import { Button } from "@/components/ui/button";
 import { createFileRoute } from "@tanstack/react-router";
 
 export const Route = createFileRoute("/")({
@@ -5,5 +6,9 @@ export const Route = createFileRoute("/")({
 });
 
 function App() {
-  return <main className="bg-green-500">Hello world</main>;
+  return (
+    <main>
+      <Button>Hello from button</Button>
+    </main>
+  );
 }
