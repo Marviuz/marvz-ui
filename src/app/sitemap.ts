52df31336af16a68393d6b33bc38cf987f178fa9
Marviuz <marviuz.dev@gmail.com>
2025-09-01 14:02:42 +0800

    fix: sitemap for homepage

--- src/app/sitemap.ts
+++ src/app/sitemap.ts
@@ -3,10 +3,18 @@ import { getBaseUrl } from '~/lib/get-base-url';
 import { registry } from '~registry/__all__';
 
 export default function sitemap(): MetadataRoute.Sitemap {
-  return Object.values(registry).map((item) => ({
-    url: `${getBaseUrl()}/docs/components/${item.name}`,
-    lastModified: new Date(),
-    changeFrequency: 'weekly' as const,
-    priority: 0.8,
-  }));
+  return [
+    {
+      url: getBaseUrl(),
+      lastModified: new Date(),
+      changeFrequency: 'daily',
+      priority: 1,
+    },
+    ...Object.values(registry).map((item) => ({
+      url: `${getBaseUrl()}/docs/components/${item.name}`,
+      lastModified: new Date(),
+      changeFrequency: 'weekly' as const,
+      priority: 0.8,
+    })),
+  ];
 }
